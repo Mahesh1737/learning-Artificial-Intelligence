ef1f1666baea7f6bf987b079cc4d9ab89f64f84b
Mahesh1737 <maheshmore1737@gmail.com>
2025-09-30 19:53:47 +0530

    learned the OneHotEncoding and preprocessing

--- Pandas/preprocessing.py
+++ Pandas/preprocessing.py
@@ -2,34 +2,32 @@ import pandas as pd
 import numpy as np
 
 #Load and do the preproccessing of the insurance dataset
-data = pd.read_csv('insurance.csv')
+data = pd.read_csv('Pandas/insurance.csv')
 print(data.head())
 
-#Check for null values
-print(data.isnull().sum())
+# Check for null values
+print("Null values:",data.isnull().sum())
 
 #Check for duplicates
-print(data.duplicated().sum())
-print(data.duplicated().sum())
+print("Duplicate rows:",data.duplicated().sum())
 
 #Check for outliers
-print(data.describe())
+print("Outliers:",data.describe())
 
 #Check for categorical variables
-print(data.dtypes)
+print("Categorical variables:\n",data.dtypes)
 
 #Encoding categorical variables
-data = pd.get_dummies(data, drop_first=True)
+data = pd.get_dummies(data, drop_first=True)    
+print("Encoded categorical features:\n", data.head())    
 
-#Feature scaling
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-scaled_data = scaler.fit_transform(data)
 
-#Convert scaled data back to dataframe
-data = pd.DataFrame(scaled_data, columns=data.columns)
-print(data.head())
+# #Feature scaling
+# from sklearn.preprocessing import OneHotEncoder
+# encoder = OneHotEncoder (sparse_output=False, drop='first') # drop first to avoid multicollinearity
+# data = encoder.fit_transform(data)
 
-#Save the preprocessed data
-data.to_csv('preprocessed_insurance.csv', index=False)
+# print("Scaled features:\n", data[:5])
 
+# #Save the preprocessed data
+data.to_csv('Pandas/insurance_preprocessed.csv', index=False)
